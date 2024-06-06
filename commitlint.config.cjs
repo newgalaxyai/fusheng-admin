2c8e0316a0ca44aaa911ef9842d96014cffdedd1
zhangshijie <2492313369@qq.com>
2024-06-06 16:11:52 +0800

    feat(规范化): 🚀 提交规范化

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -29,7 +29,8 @@ module.exports = {
         'wip',
         'workflow',
         'types',
-        'release'
+        'release',
+        'other',
       ]
     ]
   },
@@ -116,34 +117,34 @@ module.exports = {
       // 	emoji: '⏪️'
       // }
       // 中文版
-      { value: '特性', name: '特性:   🚀  新增功能', emoji: '🚀' },
-      { value: '修复', name: '修复:   🧩  修复缺陷', emoji: '🧩' },
-      { value: '文档', name: '文档:   📚  文档变更', emoji: '📚' },
+      { value: 'feat', name: '特性:   🚀  新增功能', emoji: '🚀' },
+      { value: 'fix', name: '修复:   🧩  修复缺陷', emoji: '🧩' },
+      { value: 'docs', name: '文档:   📚  文档变更', emoji: '📚' },
       {
-        value: '格式',
+        value: 'style',
         name: '格式:   🎨  代码格式（不影响功能，例如空格、分号等格式修正）',
         emoji: '🎨'
       },
       {
-        value: '重构',
+        value: 'refactor',
         name: '重构:   ♻️  代码重构（不包括 bug 修复、功能新增）',
         emoji: '♻️'
       },
-      { value: '性能', name: '性能:   ⚡️  性能优化', emoji: '⚡️' },
+      { value: 'perf', name: '性能:   ⚡️  性能优化', emoji: '⚡️' },
       {
-        value: '测试',
+        value: 'test',
         name: '测试:   ✅  添加疏漏测试或已有测试改动',
         emoji: '✅'
       },
       {
-        value: '构建',
+        value: 'build',
         name: '构建:   📦️  构建流程、外部依赖变更（如升级 npm 包、修改 webpack 配置等）',
         emoji: '📦️'
       },
-      { value: '集成', name: '集成:   🎡  修改 CI 配置、脚本', emoji: '🎡' },
-      { value: '回退', name: '回退:   ⏪️  回滚 commit', emoji: '⏪️' },
+      { value: 'ci', name: '集成:   🎡  修改 CI 配置、脚本', emoji: '🎡' },
+      { value: 'revert', name: '回退:   ⏪️  回滚 commit', emoji: '⏪️' },
       {
-        value: '其他',
+        value: 'other',
         name: '其他:   🔨  对构建过程或辅助工具和库的更改（不影响源文件、测试用例）',
         emoji: '🔨'
       }
